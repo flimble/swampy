374b4aabce66e18b14be1ca1217154fe6a007c83
John Fattal <john.fattal@foxycode.com>
2012-10-09 17:37:01 +1100

    Added MVC.NET admin page for new endpoints.

---
+++ SAIG.PS.Swampy/SAIG.PS.Swampy.Admin.MVC/Controllers/EndpointController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SAIG.PS.Swampy.Admin.MVC.Models.Operation;
+using SAIG.PS.Swampy.MongoDataAccess;
+using SAIG.PS.Swampy.Service.Entities.Endpoint;
+using SAIG.PS.Swampy.Service.QueryObjects;
+
+namespace SAIG.PS.Swampy.Admin.MVC.Controllers
+{
+    public class EndpointController : Controller
+    {
+       private readonly ISession _session;
+
+        public EndpointController(ISession session)
+        {
+            _session = session;
+        }
+
+        public ActionResult Create(string environmentName)
+        {
+            var model = new CreateEndpointOperationModel
+                {
+                    EnvironmentName = environmentName
+                };
+
+            return View(model);
+        }
+
+        public ActionResult Save(CreateEndpointOperationModel newEndpoint)
+        {
+            
+            var environment = _session.QueryOne(new EnvironmentByNameQuery
+                {
+                    EnvironmentName = newEndpoint.EnvironmentName
+                });
+
+
+            var toAdd = new SimpleEndpoint
+                {
+                    Description = newEndpoint.Description,
+                    Key = newEndpoint.Name,
+                    Value = newEndpoint.Value
+                };
+
+            environment.Endpoints.Add(toAdd);
+
+            _session.Save(environment);
+
+            return RedirectToAction("Index", "Environment", new { environmentName = newEndpoint.EnvironmentName } );
+        }
+
+    }
+}
---
+++ SAIG.PS.Swampy/SAIG.PS.Swampy.Admin.MVC/Models/Operation/CreateEndpointOperationModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAIG.PS.Swampy.Admin.MVC.Models.Operation
+{
+    public class CreateEndpointOperationModel
+    {
+        public List<string> EndpointTypes { get
+        {
+            return new List<string>{"Simple", "SQL"};    
+        }
+        } 
+
+        public string EnvironmentName { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+        public string Description { get; set; }
+    }
+}
