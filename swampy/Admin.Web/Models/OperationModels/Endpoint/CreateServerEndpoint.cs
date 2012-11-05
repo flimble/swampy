7e9cacb26709c846ba59bb784194b6855a71aa22
John Fattal <john.fattal@foxycode.com>
2012-11-05 11:57:02 +1100

    reconfirmed delete changes

--- swampy/Admin.Web/Models/OperationModels/Endpoint/CreateServerEndpoint.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Swampy.Admin.Web.Models.OperationModels.Endpoint
-{
-    public class CreateServerEndpoint : CreateEndpointBase
-    {
-        public string ServerName { get; set; }
-        public string Domain { get; set; }
-    }
-}
--- swampy/Admin.Web/Models/ReadModels/HomeReadModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Swampy.Admin.Web.Models.ReadModels
-{
-    public class HomeReadModel
-    {
-        public List<string> EnvironmentNames { get; set; }
-    }
-}
