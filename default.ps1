82f801172f25d2cb3dfc4d8ca474fb7922fcb5e2
John Fattal <john.fattal@saiglobal.com>
2013-03-21 16:53:57 +1100

    replaced psake_ext.ps1 with edamame

--- default.ps1
+++ default.ps1
@@ -12,8 +12,20 @@ properties {
 	$integrationtest_assembly_filter = '*IntegrationTest*.dll'
 }
 
-include .\psake_ext.ps1
-include .\teamcity.ps1
+$base = (resolve-path .)
+
+$nuget_dir = resolve-path '.\.nuget\'
+$nuget = join-path $nuget_dir 'nuget.exe'
+$packages_config = join-path $nuget_dir 'packages.config'
+$output_dir = join-path $base 'packages'
+& $nuget install $packages_config -OutputDirectory $output_dir -source 'http://ausydhc-pspap12.saig.frd.global:81/nuget/'
+
+$packages_dir = join-path $base 'packages'
+$edamame_dir = (get-childitem $packages_dir edamame* | select-object -last 1).fullname
+$edamame_path = join-path $edamame_dir 'tools\edamame.psm1'
+Import-Module ($edamame_path) -DisableNameChecking -force 
+
+
 
 TaskSetup {
     TeamCity-ReportBuildProgress "Running task $($psake.context.Peek().currentTaskName)"
